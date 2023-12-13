f7dbd69561c5db1f0438842cb776998cc3d32744
Breno Junqueira <breno.junqueira@estudante.ufjf.br>
2023-12-13 18:02:51 -0300

    Criacao e Implementacao de Funcoes

--- main.js
+++ main.js
@@ -5,4 +5,31 @@ const olSaida = document.querySelector('#itens')
 const btnAdicionar = document.querySelector('#adicionar')
 const btnLimpar = document.querySelector('#limpar')
 
+btnLimpar.addEventListener('click', limparElementosDaLista)
+btnAdicionar.addEventListener('click', criaElementoNaLista)
 
+atualizarItensDeLista()
+
+function criaElementoNaLista(){
+    const texto = pEntrada.textContent
+    adicionaNaLista(texto)
+    atualizarItensDeLista()
+    pEntrada.textContent=""
+    pEntrada.focus()
+}
+
+function limparElementosDaLista(){
+    limpaLista()
+    atualizarItensDeLista()
+}
+
+function atualizarItensDeLista(){
+    olSaida.innerHTML = ""
+    const lista = getLista()
+    for(let i = 0; i < lista.length; i++){
+        const item = lista[i]
+        const li = document.createElement('li')
+        li.textContent = item
+        olSaida.appendChild(li)
+    }
+}
